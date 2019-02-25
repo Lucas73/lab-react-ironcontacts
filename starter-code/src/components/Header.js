44d06e821c3ce9b9dfa8002b8956fc74b70d0a8f
Lucas <lucas@MacBook-Air-de-Lucas.local>
2019-02-25 15:08:04 +0100

    fit Header and list

---
+++ starter-code/src/components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+        <header className="App-header">
+          <h1 className="App-title">IronContacts</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Header;
